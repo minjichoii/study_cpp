5604dc4e861d3bfc196f111f8bd6a73ed6433772
최민지 <choiminji@choeminjiui-noteubug.local>
2025-02-09 22:20:09 +0900

    BOJ 9093 | 2025-02-09 edit | stack

--- baekjoon9093.cpp
+++ baekjoon9093.cpp
@@ -9,4 +9,26 @@ int main(){
     cin >> n;
     cin.ignore();
 
+    string sentence;
+    stack <char> temp;
+
+    while(n--)
+    {
+        getline(cin, sentence);
+        sentence += ' ';
+
+        for (int i=0; i<sentence.size(); i++)
+        {
+            if (sentence[i]==' ')
+            {
+                while (!temp.empty())
+                {
+                    cout << temp.top();
+                    temp.pop();
+                }cout << ' ';
+            }
+            else
+                temp.push(sentence[i]);
+        }cout << "\n";
+    }
 }
