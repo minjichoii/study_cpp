265517597a1ce2a6c1c534b6561f6f8c44ebbf3e
최민지 <cmj010202@naver.com>
2025-04-14 22:43:29 +0900

    BOJ 18258 | 250414 | queue_basic

---
+++ queue/baekjoon18258.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    queue<int> Q;
+    cin >> n;
+
+    while(n--){
+        string cmd;
+        cin >> cmd;
+
+        if (cmd == "push") {
+            int val;
+            cin >> val;
+            Q.push(val);
+        }
+        else if (cmd == "pop") {
+            if (Q.empty()) cout << -1 <<'\n';
+            else {
+                cout << Q.front() << '\n';
+                Q.pop();
+            }
+        }
+        else if (cmd == "size") {
+            cout << Q.size() << '\n';
+        }
+        else if(cmd == "empty") {
+            cout << Q.empty() << '\n';
+        }
+        else if(cmd == "front") {
+            if (Q.empty()) cout << -1 << '\n';
+            else cout << Q.front()<<'\n';
+        }
+        else if(cmd == "back") {
+            if (Q.empty()) cout << -1 << '\n';
+            else cout << Q.back() << '\n';
+        }
+    }
+}
