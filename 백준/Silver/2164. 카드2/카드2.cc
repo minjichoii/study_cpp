4395e5d0e095f25e3fe1a692fcfb156882ccd484
최민지 <89441446+minjichoii@users.noreply.github.com>
2025-09-15 19:34:01 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    queue<int> Q;
+    int N;
+    cin >> N;
+    
+    for (int i=1; i<=N; i++) {
+        Q.push(i);
+    }
+
+    while (Q.size() != 1) {
+        Q.pop();
+        Q.push(Q.front());
+        Q.pop();
+    }
+
+    cout << Q.front() << std::endl;
+    return 0;
+}
